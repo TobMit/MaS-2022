346d4dac02764a3b230bb0be987293e321bdc8b7
TobMit <80719513+tobmit@users.noreply.github.com>
2022-12-09 15:04:07 +0100

    Prepočitavadlo dát

--- Semestralka/SpracovanieDat.java
+++ Semestralka/SpracovanieDat.java
@@ -2,26 +2,29 @@ import OSPRNG.UniformDiscreteRNG;
 
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class SpracovanieDat {
     public static void main(String[] args) {
         TreeMap<Integer, Integer> diagram = new TreeMap<>();
         Scanner scanner = new Scanner(System.in);
-        UniformDiscreteRNG random = new UniformDiscreteRNG(1,60);
+        //UniformDiscreteRNG random = new UniformDiscreteRNG(1,60);
         while (scanner.hasNext()) {
-            int cislo = scanner.nextInt();
+            StringTokenizer stringTokenizer = new StringTokenizer(scanner.nextLine(),":");
+            int cislo = Integer.parseInt(stringTokenizer.nextToken());
+            int cislo2 = Integer.parseInt(stringTokenizer.nextToken());
             cislo *= 60;
-            cislo += random.sample();
-            if (cislo == 0) {
-                cislo = 1;
-            }
-            if (!diagram.containsKey(cislo)) {
-                diagram.put(cislo, 1);
-            } else {
-                int tmpCislo = diagram.get(cislo);
-                diagram.replace(cislo, tmpCislo+1);
-            }
+            cislo += cislo2;
+//            if (cislo == 0) {
+//                cislo = 1;
+//            }
+//            if (!diagram.containsKey(cislo)) {
+//                diagram.put(cislo, 1);
+//            } else {
+//                int tmpCislo = diagram.get(cislo);
+//                diagram.replace(cislo, tmpCislo+1);
+//            }
             System.out.println(cislo);
         }
 /*
