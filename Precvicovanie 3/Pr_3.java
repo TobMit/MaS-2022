58a5c32f9ce4ef1fe69593d5a7d15dae7e96c683
TobMit <80719513+tobmit@users.noreply.github.com>
2022-12-12 22:18:03 +0100

    Hotovo

--- Precvicovanie 3/Pr_3.java
+++ Precvicovanie 3/Pr_3.java
@@ -10,20 +10,39 @@ public class Pr_3 {
         boolean najdene = false;
         UniformContinuousRNG randomX = new UniformContinuousRNG();
         UniformContinuousRNG randomY = new UniformContinuousRNG();
-        long pocetPadnutych = 0;
+
+        double pocetPadnutych = 0;
+
+        // toto je na zaokruhľovanie -  takto si zýskam presne čislo zaokrúhlené na 6 desatinných miest
+        // nie je to ideálne ale funguje
         String tmp = String.format("%.6f", Math.PI);
         double pi = Double.parseDouble(tmp);
+
+        double pocetHdov = 0;
+
         while (!najdene) {
+            pocetHdov++;
             double x = randomX.sample();
             double y = randomY.sample();
+            // porovnávam či to padlo
             if ((Math.pow(x - xKruhu, 2) + Math.pow(y - yKruhu, 2)) <= Math.pow(r, 2)) {
                 pocetPadnutych++;
             }
+            double pseudoPiVypocet = 0;
 
+            if (pocetPadnutych != 0) {
+                // premenné musia byť double lebo inak by delenie bolo zaokrúhlené
+                pseudoPiVypocet = (pocetPadnutych / pocetHdov) / (r * r);
+            }
+            // znovu zaokrúhlovanie
+            String stringPseudoPi = String.format("%.6f", pseudoPiVypocet);
+            double pseudoPi = Double.parseDouble(stringPseudoPi);
+            if (pseudoPi == pi) {
+                najdene = true;
+            }
+            System.out.println(pocetHdov + " " + pocetPadnutych + " " + pi + " " + pseudoPi);
         }
-        System.out.println(Math.PI);
-        //String tmp = String.format("%.6f", Math.PI);
-        System.out.println(tmp);
-        System.out.printf("%f", Math.pow(10,-6));
+        System.out.println(pocetHdov);
+
     }
 }
