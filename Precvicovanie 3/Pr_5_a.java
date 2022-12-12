5215b9d728e12e37b17ce39968fa318ef3515b73
TobMit <80719513+tobmit@users.noreply.github.com>
2022-12-12 23:50:21 +0100

    Spravne

---
+++ Precvicovanie 3/Pr_5_a.java
@@ -0,0 +1,36 @@
+import OSPRNG.UniformContinuousRNG;
+
+public class Pr_5_a {
+    private static final int pocetReplikaci = 10000000;
+    private static double  prPravda = 0.8;
+    public static void main(String[] args) {
+        double pocetZeSaZhodliTriAPravda = 0;
+        double pocetZeSaZhodliTri = 0;
+        double pocetZeSaZhodliDve = 0;
+        double pocetZeSaZhodliDveAPravda = 0;
+
+        UniformContinuousRNG pravda1 = new UniformContinuousRNG();
+        UniformContinuousRNG pravda2 = new UniformContinuousRNG();
+        UniformContinuousRNG pravda3 = new UniformContinuousRNG();
+        for (int i = 0; i < pocetReplikaci; i++) {
+            boolean prvaPravda = pravda1.sample() < prPravda;
+            boolean druhaPravda = pravda2.sample() < prPravda;
+            boolean tretiaPravda = pravda3.sample() < prPravda;
+
+            if (prvaPravda == druhaPravda  && prvaPravda == tretiaPravda) {
+                pocetZeSaZhodliTri++;
+                if (prvaPravda) {
+                    pocetZeSaZhodliTriAPravda++;
+                }
+            }
+
+            if (prvaPravda == druhaPravda) {
+                pocetZeSaZhodliDve++;
+                if (prvaPravda) {
+                    pocetZeSaZhodliDveAPravda++;
+                }
+            }
+        }
+        System.out.println("Pravdepodobnost ze majú pravdu 2: " + pocetZeSaZhodliDveAPravda/pocetZeSaZhodliDve + " pravdepodbnost ze maju pravdu 3: " + pocetZeSaZhodliTriAPravda/pocetZeSaZhodliTri);
+    }
+}
