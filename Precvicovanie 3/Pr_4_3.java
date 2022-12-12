2a42c2535b97fb84cc84b7b565991da5d697e212
TobMit <80719513+tobmit@users.noreply.github.com>
2022-12-12 23:06:56 +0100

    Hotovo

--- Precvicovanie 3/Pr_4_3.java
+++ Precvicovanie 3/Pr_4_3.java
@@ -6,35 +6,47 @@ public class Pr_4_3 {
     public static void main(String[] args) {
         double priemernaVzdialenostX = 0;
         double priemernaVzdialenostY = 0;
+        double priemernaVzdialenostZ = 0;
         // ich knižnica je strašne pomalá
         UniformContinuousRNG random = new UniformContinuousRNG();
         for (int i = 0; i < pocetReplikacii; i++) {
             int vzdialenostOdStreduX = 0;
             int vzdialenostOdStreduY = 0;
+            int vzdialenostOdStreduZ = 0;
             for (int j = 0; j < pocetKrokov; j++) {
                 if (random.sample() < 0.5) {
                     if (random.sample() < 0.5) {
-                        vzdialenostOdStreduX++;
+                        vzdialenostOdStreduZ++;
                     } else {
-                        vzdialenostOdStreduX--;
+                        vzdialenostOdStreduZ--;
                     }
                 } else {
                     if (random.sample() < 0.5) {
-                        vzdialenostOdStreduY++;
+                        if (random.sample() < 0.5) {
+                            vzdialenostOdStreduX++;
+                        } else {
+                            vzdialenostOdStreduX--;
+                        }
                     } else {
-                        vzdialenostOdStreduY--;
+                        if (random.sample() < 0.5) {
+                            vzdialenostOdStreduY++;
+                        } else {
+                            vzdialenostOdStreduY--;
+                        }
                     }
                 }
-                priemernaVzdialenostX += Math.abs(vzdialenostOdStreduX);
-                priemernaVzdialenostY += Math.abs(vzdialenostOdStreduY);
             }
+            priemernaVzdialenostX += Math.abs(vzdialenostOdStreduX);
+            priemernaVzdialenostY += Math.abs(vzdialenostOdStreduY);
+            priemernaVzdialenostZ += Math.abs(vzdialenostOdStreduZ);
 
         }
         //Math.sqrt((Math.pow(first - druhyBod.first, 2 )) + (Math.pow(second - druhyBod.second, 2)));
         priemernaVzdialenostX /= pocetReplikacii;
         priemernaVzdialenostY /= pocetReplikacii;
-        double priemernaVzdialenost = Math.sqrt(Math.pow(priemernaVzdialenostX - 0, 2) + Math.pow(priemernaVzdialenostY - 0,2));
-        double ohadKrokov = Math.sqrt((4*pocetKrokov)/(Math.PI));
-        System.out.println("Priemerna vzdialenost je: " + priemernaVzdialenost / pocetReplikacii + " teoretická vzdialenost je: " + ohadKrokov);
+        priemernaVzdialenostZ /= pocetReplikacii;
+        double priemernaVzdialenost = priemernaVzdialenostX + priemernaVzdialenostY + priemernaVzdialenostZ;
+        double ohadKrokov = Math.sqrt((6*pocetKrokov)/(Math.PI));
+        System.out.println("Priemerna vzdialenost je: " + priemernaVzdialenost + " teoretická vzdialenost je: " + ohadKrokov);
     }
 }
